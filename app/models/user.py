09f524cf0fe63e592985d1dd174af341b75ba937
Vinicius Teixeira Santiago <viniciustsantiagosud@gmail.com>
2025-11-09 19:54:51 -0300

    FInalizado a criação do Users no models e no Schemas, e migration

--- app/models/user.py
+++ app/models/user.py
@@ -1,5 +1,10 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Enum, DateTime, func
 from app.db.base import Base
+import enum
+
+class RoleEnum(enum.Enum):
+    ADMIN = "ADMIN"
+    USER = "USER"
 
 class User(Base):
     __tablename__ = "users"
@@ -7,3 +12,6 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, nullable=False)
     email = Column(String, unique=True, index=True, nullable=False)
+    password_hash = Column(String, nullable=False)
+    role = Column(Enum(RoleEnum), default=RoleEnum.USER, nullable=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
